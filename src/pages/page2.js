379dcaa199945c536fcbd3d8b8eb8f78600403b2
Kelvin <kelvinsanchez15@gmail.com>
2024-03-08 06:18:39 -0600

    use crime viz

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -3,6 +3,47 @@ import Script from "next/script";
 import Link from "next/link";
 import { TableauEmbed } from "@stoddabr/react-tableau-embed-live";
 
+const VizURLs = {
+  "CR-5": {
+    dev: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard11?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard11?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-6": {
+    dev: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard10?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard10?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-7": {
+    dev: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard12_1?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/Crimes_17059390129380/Dashboard12_1?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-9": {
+    dev: "https://public.tableau.com/views/CrimeVictim_17059391178000/Dashboard2?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/CrimeVictim_17059391178000/Dashboard2?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-12": {
+    dev: "https://public.tableau.com/views/CrimeOffender_17059391850310/Dashboard2?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/CrimeOffender_17059391850310/Dashboard2?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-14": {
+    dev: "https://public.tableau.com/views/CrimeOffender_17059391850310/Dashboard3?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/CrimeOffender_17059391850310/Dashboard3?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-15": {
+    dev: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard5?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard5?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-16": {
+    dev: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard6?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard6?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+  "CR-17": {
+    dev: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard7?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+    prod: "https://public.tableau.com/views/ArrestDisparities_17059398297550/Dashboard7?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link",
+  },
+};
+
+const isProdEnv = false;
+
 function TestPage2() {
   return (
     <>
@@ -29,9 +70,27 @@ function TestPage2() {
           src='https://public.tableau.com/views/DescendantsoftheDragon/Map?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link'
         /> */}
 
-        <TableauEmbed sourceUrl='https://public.tableau.com/views/AnimalRights_17092289911360/AnimalRights?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link' />
+        {/* <TableauEmbed sourceUrl='https://public.tableau.com/views/AnimalRights_17092289911360/AnimalRights?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link' />
         <TableauEmbed sourceUrl='https://public.tableau.com/views/SanDiegosBlackPopulation/Viz?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link' />
-        <TableauEmbed sourceUrl='https://public.tableau.com/views/DescendantsoftheDragon/Map?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link' />
+        <TableauEmbed sourceUrl='https://public.tableau.com/views/DescendantsoftheDragon/Map?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link' /> */}
+
+        <TableauEmbed sourceUrl={VizURLs["CR-5"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-6"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-7"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-9"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-12"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-14"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-15"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-16"][isProdEnv ? "prod" : "dev"]} />
+
+        <TableauEmbed sourceUrl={VizURLs["CR-17"][isProdEnv ? "prod" : "dev"]} />
       </div>
     </>
   );
