93ee126ed6d1240f0d830af0d45f9c11a07e63e7
Kelvin <kelvinsanchez15@gmail.com>
2024-03-08 06:08:39 -0600

    use error boundary

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
